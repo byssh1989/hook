780484c2a5b5ed62eb7e45471e733019e9814879
gaopengfei <gaopengfei@soyoung.com>
2019-10-22 10:56:10 +0800

    基本解析

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package github_hook
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +21,36 @@ func Start() {
 
 func PushHookHandler(c *gin.Context) {
 	data, _ := c.GetRawData()
-	fmt.Printf("%s \n", data)
+	// fmt.Printf("%s \n", data)
 
+	params := GITHUB_HOOK{}
+	json.Unmarshal(data, &params)
+
+	fmt.Printf("输出: %v \n", params)
+}
+
+type GITHUB_HOOK struct {
+	Zen    string
+	HookID int
+	Hook   struct {
+		Type   string
+		ID     int
+		Name   string
+		Active bool
+		Events []string
+	}
+	Repository struct {
+		ID       int
+		NodeID   string `json:"node_id"`
+		Name     string
+		FullName string `json:"full_name"`
+		Private  bool
+	}
+	Sender struct {
+		ID        int
+		Login     string
+		NodeID    string
+		Type      string
+		SiteAdmin bool
+	}
 }
